79bb6d0ca6ee3e0a136a5a3210fba0569f604651
Vincent Edwards <mister.man489@gmail.com>
2024-12-17 15:37:15 -0800

    Switch double quotes to single
    This change was a suggestion from Rubocop, since no string interpolation
    or special symbols were needed.

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
-ALPHABET_LENGTH = "z".ord - "a".ord + 1
+ALPHABET_LENGTH = 'z'.ord - 'a'.ord + 1
 
 def caesar_cipher(message, shift)
-  characters = message.split("")
+  characters = message.split('')
 
   shifted_characters = characters.map do |character|
     if character.match?(/[a-z]/i)
-      starting_ord = (character == character.upcase) ? "A".ord : "a".ord
+      starting_ord = (character == character.upcase) ? 'A'.ord : 'a'.ord
       new_ord = (character.ord - starting_ord + shift) % ALPHABET_LENGTH + starting_ord
       new_ord.chr
     else
@@ -15,5 +15,5 @@ def caesar_cipher(message, shift)
     end
   end
 
-  shifted_characters.join("")
+  shifted_characters.join('')
 end
